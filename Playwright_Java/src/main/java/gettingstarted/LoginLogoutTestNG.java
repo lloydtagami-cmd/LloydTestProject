ac4cfe219b2551e06bfdf9d959cf2ae0e875b75a
COLLABERA.DIGITAL <COLLABERA.DIGITAL@COLLABPH1002326>
2026-03-11 13:05:44 +0800

    testing

---
+++ Playwright_Java/src/main/java/gettingstarted/LoginLogoutTestNG.java
@@ -0,0 +1,69 @@
+package gettingstarted;
+import java.util.regex.Pattern;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginLogoutTestNG {
+	@Test
+	public void loginTest() {
+		Browser browser = null;
+		Page page = null;
+		Playwright pw = null;
+		BrowserType browserType = null;
+		try {
+			pw = Playwright.create();
+			
+			browserType = pw.chromium();
+			
+			browser = Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
+			
+			page = browser.newPage();
+			
+			page.navigate("https://freelance-learn-automation.vercel.app/login");
+			
+			//PlaywrightAssertions.assertThat(page).hasTitle("Learn Automation Courses");
+			
+			Assert.assertTrue(page.title().contains("Learn Automation Courses"));
+			//by id
+			//page.locator("#email1").fill("admin@email.com");
+			//by xpath
+			//page.locator("xpath=//input[@name='email1']").fill("sss@email.com");
+			//by placeholder
+			page.getByPlaceholder("Enter Email").fill("admin@email.com");
+		
+			page.getByPlaceholder("Enter Password").fill("admin@123");
+			
+			page.getByText("Sign in").last().click();
+			
+			PlaywrightAssertions.assertThat(page.locator(".welcomeMessage")).containsText("Welcome");
+			
+			page.getByAltText("menu").click();
+			
+			page.getByText("Sign out").last().click();
+			
+			PlaywrightAssertions.assertThat(page).hasURL(Pattern.compile("login"));
+			
+			//page.waitForTimeout(3000);
+			
+		}
+		finally {
+			page.close();
+			
+			browser.close();
+			
+			pw.close();
+			
+		}
+		
+		
+		
+	}
+
+}
